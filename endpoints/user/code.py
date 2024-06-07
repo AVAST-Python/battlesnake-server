8f30fdf1ff1d2558683e0b27e1b159a93da09a6e
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-06-07 13:32:00 +0200

    Some cleanup

--- endpoints/user/code.py
+++ endpoints/user/code.py
@@ -19,19 +19,4 @@ def code(user):
         except SyntaxError as e:
             error = e.msg
 
-        # render_template('code.html', code=users_code[user])
-
     return render_template('code.html', code=users_code[user], error=error)
-
-
-@user.route('/code_old/<string:user>', methods=['GET', 'POST'])
-def code_old(user):
-    if user not in users_code:
-        users_code[user] = ''
-
-    if request.method == 'POST':
-        code = request.form['code']
-        users_code[user] = code
-        render_template('code_old.html', code=users_code[user])
-
-    return render_template('code_old.html', code=users_code[user])
